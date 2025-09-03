928c4c402e5e923da7a45c5f4fb84aa9fda54397
Jinvic <77521861+Jinvic@users.noreply.github.com>
2025-09-03 15:04:53 +0800

    Create CF520B 1400 【逆向】.go

---
+++ 基础/贪心/随手/CF520B 1400 【逆向】.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// var reader *bufio.Reader
+var writer *bufio.Writer
+var scanner *bufio.Scanner
+
+func init() {
+	in := os.Stdin
+	out := os.Stdout
+
+	// go run . -t
+	for _, arg := range os.Args {
+		if arg == "-t" ||
+			arg == "--test" {
+			in, _ = os.Open("input.txt")
+			out, _ = os.Create("output.txt")
+		}
+	}
+
+	// reader = bufio.NewReader(in)
+	writer = bufio.NewWriter(out)
+	scanner = bufio.NewScanner(in)
+	scanner.Split(bufio.ScanWords)
+
+}
+
+func main() {
+	defer writer.Flush()
+
+	// logic
+	n, m := NextInt(), NextInt()
+	res := 0
+	for m > n {
+		if m%2 == 1 {
+			m++
+		} else {
+			m /= 2
+		}
+		res++
+	}
+	res += n - m
+	PrintIntLn(res)
+}
+
+func NextString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func NextInt() int {
+	scanner.Scan()
+	x, _ := strconv.Atoi(scanner.Text())
+	return x
+}
+
+func NextInt64() int64 {
+	scanner.Scan()
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	return x
+}
+
+func NextFloat64() float64 {
+	scanner.Scan()
+	x, _ := strconv.ParseFloat(scanner.Text(), 64)
+	return x
+}
+
+func PrintInt(n int) {
+	writer.WriteString(strconv.Itoa(n))
+}
+
+func PrintInt64(n int64) {
+	writer.WriteString(strconv.FormatInt(n, 10))
+}
+
+func PrintFloat64(f float64, prec int) {
+	writer.WriteString(strconv.FormatFloat(f, 'f', prec, 64))
+}
+
+func PrintString(s string) {
+	writer.WriteString(s)
+}
+
+func PrintIntLn(n int) {
+	PrintInt(n)
+	writer.WriteByte('\n')
+}
+
+func PrintInt64Ln(n int64) {
+	PrintInt64(n)
+	writer.WriteByte('\n')
+}
+
+func PrintFloat64Ln(f float64, prec int) {
+	PrintFloat64(f, prec)
+	writer.WriteByte('\n')
+}
+
+func PrintStringLn(s string) {
+	PrintString(s)
+	writer.WriteByte('\n')
+}
