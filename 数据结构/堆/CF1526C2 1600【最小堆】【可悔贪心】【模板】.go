57aa70adb2c8f1891effb49fa42dbfaebd46bf3e
Jinvic <77521861+Jinvic@users.noreply.github.com>
2025-09-05 17:53:25 +0800

    Create CF1526C2 1600【最小堆】【可悔贪心】【模板】.go

---
+++ 数据结构/堆/CF1526C2 1600【最小堆】【可悔贪心】【模板】.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"os"
+	"strconv"
+)
+
+// var reader *bufio.Reader
+var writer *bufio.Writer
+var scanner *bufio.Scanner
+
+func init() {
+	in := os.Stdin
+	out := os.Stdout
+
+	// go run . -t
+	for _, arg := range os.Args {
+		if arg == "-t" ||
+			arg == "--test" {
+			in, _ = os.Open("input.txt")
+			out, _ = os.Create("output.txt")
+		}
+	}
+
+	// reader = bufio.NewReader(in)
+	writer = bufio.NewWriter(out)
+	scanner = bufio.NewScanner(in)
+	scanner.Split(bufio.ScanWords)
+
+}
+
+type MinHeap []int64
+
+func InitMinHeap(n int) MinHeap {
+	return make([]int64, 0, n)
+}
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x any) {
+	*h = append(*h, x.(int64))
+}
+
+func (h *MinHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// not heap.Interface
+func (h MinHeap) Top() int64 { return h[0] }
+
+func main() {
+	defer writer.Flush()
+
+	// logic
+	n := NextInt()
+	sum := int64(0)
+	res := 0
+	h := InitMinHeap(n)
+	for range n {
+		a := NextInt64()
+		if a >= 0 {
+			sum += a
+			res++
+		} else {
+			// a < 0
+			if sum+a >= 0 {
+				sum += a
+				res++
+				heap.Push(&h, a)
+			} else {
+				// sum+a < 0
+				if len(h) > 0 && a > h.Top() {
+					sum -= heap.Pop(&h).(int64)
+					sum += a
+					heap.Push(&h, a)
+				}
+			}
+		}
+	}
+
+	PrintIntLn(res)
+}
+
+func NextString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func NextInt() int {
+	scanner.Scan()
+	x, _ := strconv.Atoi(scanner.Text())
+	return x
+}
+
+func NextInt64() int64 {
+	scanner.Scan()
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	return x
+}
+
+func NextFloat64() float64 {
+	scanner.Scan()
+	x, _ := strconv.ParseFloat(scanner.Text(), 64)
+	return x
+}
+
+func PrintInt(n int) {
+	writer.WriteString(strconv.Itoa(n))
+}
+
+func PrintInt64(n int64) {
+	writer.WriteString(strconv.FormatInt(n, 10))
+}
+
+func PrintFloat64(f float64, prec int) {
+	writer.WriteString(strconv.FormatFloat(f, 'f', prec, 64))
+}
+
+func PrintString(s string) {
+	writer.WriteString(s)
+}
+
+func PrintIntLn(n int) {
+	PrintInt(n)
+	writer.WriteByte('\n')
+}
+
+func PrintInt64Ln(n int64) {
+	PrintInt64(n)
+	writer.WriteByte('\n')
+}
+
+func PrintFloat64Ln(f float64, prec int) {
+	PrintFloat64(f, prec)
+	writer.WriteByte('\n')
+}
+
+func PrintStringLn(s string) {
+	PrintString(s)
+	writer.WriteByte('\n')
+}
