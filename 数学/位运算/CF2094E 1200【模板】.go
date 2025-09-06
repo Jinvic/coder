3f53be1d6c2cdb99011fc2050059a7abfea26385
Jinvic <77521861+Jinvic@users.noreply.github.com>
2025-09-06 17:02:33 +0800

    Create CF2094E 1200【模板】.go

---
+++ 数学/位运算/CF2094E 1200【模板】.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// var reader *bufio.Reader
+var writer *bufio.Writer
+var scanner *bufio.Scanner
+
+func init() {
+	in := os.Stdin
+	out := os.Stdout
+
+	// go run . -t
+	for _, arg := range os.Args {
+		if arg == "-t" ||
+			arg == "--test" {
+			in, _ = os.Open("input.txt")
+			out, _ = os.Create("output.txt")
+		}
+	}
+
+	// reader = bufio.NewReader(in)
+	writer = bufio.NewWriter(out)
+	scanner = bufio.NewScanner(in)
+
+	// 扩大缓冲区
+	const MaxTokenLength = 300000
+	scanner.Buffer(make([]byte, MaxTokenLength), MaxTokenLength)
+
+	// 分词读入
+	scanner.Split(bufio.ScanWords)
+
+}
+
+func main() {
+	defer writer.Flush()
+
+	// logic
+	t := NextInt()
+	a := make([]int64, 200005)
+	for range t {
+		n := NextInt()
+		count := make([][2]int64, 35)
+		for i := range n {
+			a[i] = NextInt64()
+			for j := range 30 {
+				if ((a[i] >> j) & 1) == 1 {
+					count[j][1]++
+				} else {
+					count[j][0]++
+				}
+			}
+		}
+
+		res := int64(0)
+		for i := range n {
+			sum := int64(0)
+			for j := range 30 {
+				if ((a[i] >> j) & 1) == 1 {
+					sum += count[j][0] * (1 << j)
+				} else {
+					sum += count[j][1] * (1 << j)
+				}
+			}
+			res = max(res, sum)
+		}
+
+		PrintInt64Ln(res)
+	}
+
+}
+
+func NextString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func NextInt() int {
+	scanner.Scan()
+	x, _ := strconv.Atoi(scanner.Text())
+	return x
+}
+
+func NextInt64() int64 {
+	scanner.Scan()
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	return x
+}
+
+func NextFloat64() float64 {
+	scanner.Scan()
+	x, _ := strconv.ParseFloat(scanner.Text(), 64)
+	return x
+}
+
+func PrintInt(n int) {
+	writer.WriteString(strconv.Itoa(n))
+}
+
+func PrintInt64(n int64) {
+	writer.WriteString(strconv.FormatInt(n, 10))
+}
+
+func PrintFloat64(f float64, prec int) {
+	writer.WriteString(strconv.FormatFloat(f, 'f', prec, 64))
+}
+
+func PrintString(s string) {
+	writer.WriteString(s)
+}
+
+func PrintIntLn(n int) {
+	PrintInt(n)
+	writer.WriteByte('\n')
+}
+
+func PrintInt64Ln(n int64) {
+	PrintInt64(n)
+	writer.WriteByte('\n')
+}
+
+func PrintFloat64Ln(f float64, prec int) {
+	PrintFloat64(f, prec)
+	writer.WriteByte('\n')
+}
+
+func PrintStringLn(s string) {
+	PrintString(s)
+	writer.WriteByte('\n')
+}
