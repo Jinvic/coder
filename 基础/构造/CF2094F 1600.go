6a52ab6ce2cfa9a41500e5d1efd769f8ac79990f
Jinvic <77521861+Jinvic@users.noreply.github.com>
2025-09-08 09:13:05 +0800

    Create CF2094F 1600.go

---
+++ 基础/构造/CF2094F 1600.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// var reader *bufio.Reader
+var writer *bufio.Writer
+var scanner *bufio.Scanner
+
+func init() {
+	in := os.Stdin
+	out := os.Stdout
+
+	// go run . -t
+	for _, arg := range os.Args {
+		if arg == "-t" ||
+			arg == "--test" {
+			in, _ = os.Open("input.txt")
+			out, _ = os.Create("output.txt")
+		}
+	}
+
+	// reader = bufio.NewReader(in)
+	writer = bufio.NewWriter(out)
+	scanner = bufio.NewScanner(in)
+
+	// 扩大缓冲区
+	const MaxTokenLength = 300000
+	scanner.Buffer(make([]byte, MaxTokenLength), MaxTokenLength)
+
+	// 分词读入
+	scanner.Split(bufio.ScanWords)
+
+}
+
+/*
+- 当m%k==0时，每行输出k个数，同时给一个offset使上下相邻的数不相等。
+- 当m%k!=0时，循环依次输出1到k就行。
+*/
+
+func main() {
+	defer writer.Flush()
+
+	// logic
+	t := NextInt()
+	for range t {
+		n, m, k := NextInt(), NextInt(), NextInt()
+		if m%k == 0 {
+			for i := range n {
+				for j := range m {
+					x := ((j%k)+i)%k + 1
+					PrintInt(x)
+					if j == m-1 {
+						PrintStringLn("")
+					} else {
+						PrintString(" ")
+					}
+				}
+			}
+		} else {
+			cnt := 0
+			for range n {
+				for j := range m {
+					x := (cnt % k) + 1
+					cnt++
+					PrintInt(x)
+					if j == m-1 {
+						PrintStringLn("")
+					} else {
+						PrintString(" ")
+					}
+				}
+			}
+		}
+	}
+
+}
+
+func NextString() string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func NextInt() int {
+	scanner.Scan()
+	x, _ := strconv.Atoi(scanner.Text())
+	return x
+}
+
+func NextInt64() int64 {
+	scanner.Scan()
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	return x
+}
+
+func NextFloat64() float64 {
+	scanner.Scan()
+	x, _ := strconv.ParseFloat(scanner.Text(), 64)
+	return x
+}
+
+func PrintInt(n int) {
+	writer.WriteString(strconv.Itoa(n))
+}
+
+func PrintInt64(n int64) {
+	writer.WriteString(strconv.FormatInt(n, 10))
+}
+
+func PrintFloat64(f float64, prec int) {
+	writer.WriteString(strconv.FormatFloat(f, 'f', prec, 64))
+}
+
+func PrintString(s string) {
+	writer.WriteString(s)
+}
+
+func PrintIntLn(n int) {
+	PrintInt(n)
+	writer.WriteByte('\n')
+}
+
+func PrintInt64Ln(n int64) {
+	PrintInt64(n)
+	writer.WriteByte('\n')
+}
+
+func PrintFloat64Ln(f float64, prec int) {
+	PrintFloat64(f, prec)
+	writer.WriteByte('\n')
+}
+
+func PrintStringLn(s string) {
+	PrintString(s)
+	writer.WriteByte('\n')
+}
